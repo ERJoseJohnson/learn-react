133d5000b1f1c99f4d236640772e0eafb39ae561
ERJoseJohnson <jose.johnson.jose@gmail.com>
2022-01-29 17:03:51 +0800

    feat: Add custom component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Remember to import your custom component as an object
+import Item from './components/Item'
+
+/*
+ This is the main function that kicks all of your React code off. 
+ It is the top of the component render tree. All other components will appear below this App component
+
+ This function is rendered in index.html to the 'root' HTML element via the ReactDOM
+ 
+*/
 function App() {
   return (
     <div className="App">
@@ -9,6 +19,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>That's right, he's here</p>
+
+        {/* Use the imported custom component as such */}
+        <Item></Item>
+
+        
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Item.js
@@ -0,0 +1,12 @@
+
+/* 
+This is a simple definition of a custom component.
+
+A component is basically a function that returns some HTML code for react to render on a HTML document.
+Remember to export your component so that your root App.js file can import this component
+*/
+function Item(params) {
+    return <h2>This is an item!</h2>
+}
+
+export default Item;
